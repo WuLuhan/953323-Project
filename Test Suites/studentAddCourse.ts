6745c84769b3e4f77bc0afe77d4f3346a5edb6b4
WuLuhan <36782046+WuLuhan@users.noreply.github.com>
2018-03-24 19:39:32 +0800

    Submit all studentAddCourse test file to github

---
+++ Test Suites/studentAddCourse.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>student add course testing</description>
+   <name>studentAddCourse</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-24T19:23:00</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>39395c67-0a7a-44e7-8da8-f45b81dbef62</testSuiteGuid>
+   <testCaseLink>
+      <guid>61d9d85f-f95a-4aca-940c-5a20b395eea9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Stu_addCourse test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
