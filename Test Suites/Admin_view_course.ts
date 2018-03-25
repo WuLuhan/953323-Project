3bfb04009033ac8e55c13c252ef4e88053b71565
XU <XU@10.82.212.52>
2018-03-25 22:42:06 +0800

    Admin_view_course

---
+++ Test Suites/Admin_view_course.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Admin_view_course</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-25T22:37:48</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>41b5537c-694a-4959-a207-bc93c7e34858</testSuiteGuid>
+   <testCaseLink>
+      <guid>cd0e7a9d-6bc0-43ce-a727-e9cc3d30db90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Admin_view_course</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
