e6d51b3c4b61f3ec95dff9f2aca5ab253f2c1b50
XU <XU@10.82.212.52>
2018-03-25 22:27:37 +0800

    delete

---
+++ Test Suites/Administrator delete system.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Administrator delete system</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-25T22:25:04</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a7b2e677-8e62-417a-b0c0-248123fb273d</testSuiteGuid>
+   <testCaseLink>
+      <guid>5e4d8c73-dd79-4ef1-b176-ad35325823dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Administrator delete system</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
