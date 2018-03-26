eb819cbcd637e2966c1f5a1fcd43f0982cd75c0b
huttonwen <785133021@qq.com>
2018-03-26 12:42:38 +0800

    Admin_add_TeaInfo

---
+++ Test Suites/Admin_add_TeaInfo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Admin_add_TeaInfo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-26T12:41:04</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0dd9619c-2370-45b2-9939-3bd39859ef85</testSuiteGuid>
+   <testCaseLink>
+      <guid>95be2b68-295b-4108-bba5-45242141565a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Admin_add_TeaInfo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
