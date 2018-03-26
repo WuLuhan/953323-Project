30f102a6930da661f074cffb6871395733eb6365
WuLuhan <36782046+WuLuhan@users.noreply.github.com>
2018-03-26 13:13:00 +0800

    student withdrawal course

---
+++ Test Suites/studentWithdrawalCourse.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>studentWithdrawalCourse</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-26T13:10:44</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>408fc8fc-d6eb-4064-b6ca-d20108da15fc</testSuiteGuid>
+   <testCaseLink>
+      <guid>5922f832-b853-41ff-8e04-9c197717d2a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Stu_withdrawalCourse tesst</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
