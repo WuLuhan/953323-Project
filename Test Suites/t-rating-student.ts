77566404b8b8b8394feb0072982e97b41acc95fa
ZhenZhen-Qin <qinzz2018@gmail.com>
2018-03-28 17:30:33 +0800

    t-rating

---
+++ Test Suites/t-rating-student.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>t-rating-student</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T17:26:08</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>48c4ca80-32e1-448c-a423-c0fff3b42cef</testSuiteGuid>
+   <testCaseLink>
+      <guid>8952dd34-028f-47a2-a9cc-202657c41dc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/t-rating-student</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
