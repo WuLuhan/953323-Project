891a4aa43feaeeca1f994e8a3fa328581a3097f6
ZhenZhen-Qin <qinzz2018@gmail.com>
2018-03-28 17:41:44 +0800

    t-select-student

---
+++ Test Suites/t-select-student.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>t-select-student</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T17:38:20</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d8ef4904-e1b2-4bda-981e-9b3dc3a54f68</testSuiteGuid>
+   <testCaseLink>
+      <guid>0c0ea082-04b5-450c-a9e1-9eb9d29a307b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/t-select-student</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
