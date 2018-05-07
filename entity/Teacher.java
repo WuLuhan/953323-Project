5a54b2ca854ac3b34a6b2fe824b137068b5fb9f2
ZhenZhen-Qin <36883485+ZhenZhen-Qin@users.noreply.github.com>
2018-05-07 14:13:54 +0700

    Add files via upload

---
+++ entity/Teacher.java
@@ -0,0 +1,43 @@
+package com.qinzz.edu.entity;
+
+public class Teacher {
+	
+		public String teacherName;
+		public String teacherId;
+		public String teacherNation;
+		public int teacherAge;
+		public int teacherIDcard;
+		public String getTeacherName() {
+			return teacherName;
+		}
+		public void setTeacherName(String teacherName) {
+			this.teacherName = teacherName;
+		}
+		public String getTeacherId() {
+			return teacherId;
+		}
+		public void setTeacherId(String teacherId) {
+			this.teacherId = teacherId;
+		}
+		public String getTeacherNation() {
+			return teacherNation;
+		}
+		public void setTeacherNation(String teacherNation) {
+			this.teacherNation = teacherNation;
+		}
+		public int getTeacherAge() {
+			return teacherAge;
+		}
+		public void setTeacherAge(int teacherAge) {
+			this.teacherAge = teacherAge;
+		}
+		public int getTeacherIDcard() {
+			return teacherIDcard;
+		}
+		public void setTeacherIDcard(int teacherIDcard) {
+			this.teacherIDcard = teacherIDcard;
+		}
+		
+		
+	}
+
